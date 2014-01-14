b7e43f592807910adede7ef06bc6a23cd36af461
Elizabeth Mitchell <emitchell421@yahoo.com>
2014-01-13 22:43:16 -0500

    Clean up js file.

--- js/screen.js
+++ js/screen.js
@@ -9,75 +9,77 @@
   $(document).ready (function() {
      
     resizableBox();
-    
     overrideEnter();
     
   });
   
-// When user submits site, height, and width, site displays in iframe.
-	  
-	function userSubmit() {
-		
-    var $uurl = $("#uurl").val();
+// UPDATE IFRAMES
+
+  // MULTI SCREEN SUBMIT: When a user adds site, 3 iframes all update of different set screen sizes.
+
+    function multiSubmit() {
     
-    var $uwidth = $("#dWidth").val() + "px";
-    var $uheight = $("#dHeight").val() + "px";
+      var $murl = $("#murl").val();
+
+      // Update the url.
+      $(".multiScreen").attr("src", $murl);
     
-		// Update the url.
-		$("#displayPage").attr("src", $uurl);
-		
-		// Update the width and height of the frame.
-		$("#displayPage").attr("width", $uwidth);
-		$("#displayPage").attr("height", $uheight);
-		
-	};
+    };
 
-// In a separate section, when a user adds site, 3 iframes all update of different set screen sizes.
+      
+  // RESIZABLE SUBMIT: When user adds site, it displays in a resizable iframe.
 
-
-  function multiSubmit() {
-  
-    var $murl = $("#murl").val();
-
-		// Update the url.
-		$(".multiScreen").attr("src", $murl);
-  
-  };
-
+    function resizableSubmit() {
     
-// In a separate section, when user adds site, it displays in a resizable iframe.
-
-  function resizableSubmit() {
-  
-    var $rurl = $("#rurl").val();
-	
-		// Update the url.
-		$("#rDisplay").attr("src", $rurl);
-  
-  };
- 
- 
-// Create resizable iframe.
-  
-  function resizableBox() {
-  
-    $("#rescaleDiv").resizable({
-      alsoResize: '#rDisplay'
-    });
+      var $rurl = $("#rurl").val();
+    
+      // Update the url.
+      $("#rDisplay").attr("src", $rurl);
+    
+    };
+   
+   
+  // BASIC/USER-DEFINED SCREEN: When user submits site, height, and width, site displays in iframe.
+      
+    function userSubmit() {
+      
+      // Variables
+      var $uurl = $("#uurl").val();
+      var $uwidth = $("#dWidth").val() + "px";
+      var $uheight = $("#dHeight").val() + "px";
+      
+      // Update the url.
+      $("#displayPage").attr("src", $uurl);
+      
+      // Update the width and height of the frame.
+      $("#displayPage").attr("width", $uwidth);
+      $("#displayPage").attr("height", $uheight);
+      
     };
     
-    
-// Handle enter key.
+// ADDITIONAL FUNCTIONS
  
-  // If enter is pushed, override default behavior.
-  
-  function overrideEnter() {
-  
-    var $textBox = $("input[type='text']");    
+  // Create resizable iframe.
     
-    $textBox.on('keypress', function(e) {
-      if (e.which == 13) {
-        e.preventDefault();
-      }
-    });
-  };
+    function resizableBox() {
+    
+      $("#rescaleDiv").resizable({
+        alsoResize: '#rDisplay'
+      });
+      };
+    
+    
+  // Handle enter key.
+   
+    // If enter is pushed, override default behavior.
+    
+    function overrideEnter() {
+    
+      var $textBox = $("input[type='text']");    
+      
+      $textBox.on('keypress', function(e) {
+        if (e.which == 13) {
+          e.preventDefault();
+        }
+      });
+    };
